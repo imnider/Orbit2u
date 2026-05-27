d4996cf9ffbbc9cae84e5cdf7415ecc826912515
imnider <velezzambranoneider@gmail.com>
2026-05-27 13:53:53 -0500

    Revert "Merge branch 'develop-nvelez' into development"
    This reverts commit 3f17d71a048618cdb45e9912bc40dfb4253b8176, reversing
    changes made to aacc531a1bceb1bc24f95ff407e58ac45e10891c.

--- backend/YoutubeClone.Shared/SMTP.cs
+++ backend/YoutubeClone.Shared/SMTP.cs
@@ -12,7 +12,7 @@ namespace YoutubeClone.Shared
                 Host = host,
                 Credentials = new NetworkCredential(user, password),
                 Port = port,
-                EnableSsl = true
+                EnableSsl = false
             };
 
             var message = new MailMessage(from, to, subject, body)
