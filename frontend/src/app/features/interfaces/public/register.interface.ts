89c11feaa968ce1b20ecb72b2775a862d2b9b6c1
Juliet <julietcaro40@gmail.com>
2026-05-30 22:18:21 -0500

    fix: registration process corrected and functional

--- frontend/src/app/features/interfaces/public/register.interface.ts
+++ frontend/src/app/features/interfaces/public/register.interface.ts
@@ -1,12 +1,18 @@
-export interface RegisterRequest {
-    username: string;
-    email: string;
-    displayName: string;
-    birthday: string;
-    location: string;
+// paso 1 - devuelve email
+export interface ValidateTokenResponse {
+  email: string;
 }
-
-export interface RegisterResponse {
-    token: string;
-    refreshToken: string;
+ 
+// paso 2 - body de post
+export interface CompleteRegisterRequest {
+  username: string;
+  displayName: string;
+  birthday: string;
+  location: string;
+}
+ 
+// paso 3 - respuesta complete
+export interface CompleteRegisterResponse {
+  token: string;
+  refreshToken: string;
 }
