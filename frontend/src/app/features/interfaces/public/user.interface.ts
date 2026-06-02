4302ff9f0f756074c1ba5cb8b045850682536dbd
imnider <velezzambranoneider@gmail.com>
2026-06-02 12:09:21 -0500

    fix: membershipPlan cant be null

--- frontend/src/app/features/interfaces/public/user.interface.ts
+++ frontend/src/app/features/interfaces/public/user.interface.ts
@@ -1,52 +1,52 @@
-import { ApiResponse } from "./api-response.interface";
+import { ApiResponse } from './api-response.interface';
 
 export interface Role {
-    id: string;
-    name: string;
-    description: string | null;
+  id: string;
+  name: string;
+  description: string | null;
 }
 
-export interface MembershipPlan{
-    membershipPlanId: number;
-    displayName: string;
-    description: string | null;
-    monthlyPrice: number;
-    coinsReward: number;
-    maxCommunities: number;
-    maxVideosPerCommunity: number;
+export interface MembershipPlan {
+  membershipPlanId: number;
+  displayName: string;
+  description: string | null;
+  monthlyPrice: number;
+  coinsReward: number;
+  maxCommunities: number;
+  maxVideosPerCommunity: number;
 }
 
 export interface CurrentUser {
-    userId: string;
-    userName: string;
-    displayName: string;
-    email: string;
-    birthday: string;
-    location: string;
-    password: string;
-    createdAt: string;
-    deletedAt: string | null;
-    role: Role | null;
-    membershipPlan: MembershipPlan | null;
+  userId: string;
+  userName: string;
+  displayName: string;
+  email: string;
+  birthday: string;
+  location: string;
+  password: string;
+  createdAt: string;
+  deletedAt: string | null;
+  role: Role | null;
+  membershipPlan: MembershipPlan;
 }
 
 export interface UpdateUserRequest {
-    userName?: string | null;
-    displayName?: string | null;
-    email?: string | null;
-    birthday?: string | null;
-    location?: string | null;
-    roleId?: string | null;
-    membershipPlanId?: number | null; //?
+  userName?: string | null;
+  displayName?: string | null;
+  email?: string | null;
+  birthday?: string | null;
+  location?: string | null;
+  roleId?: string | null;
+  membershipPlanId?: number | null; //?
 }
 
 export interface AddCoinsRequest {
-    coinPackageId: number;
+  coinPackageId: number;
 }
 
 export interface ChangePasswordRequest {
-    currentPassword: string;
-    newPassword: string;
+  currentPassword: string;
+  newPassword: string;
 }
 
 export type MeResponse = ApiResponse<CurrentUser>;
